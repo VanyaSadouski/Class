18fff4db6b9f482f9f6e5304ffe2850b368323d7
VanyaSadouski <34644859+VanyaSadouski@users.noreply.github.com>
2018-02-10 15:33:51 +0300

    hw3

--- ES5.js
+++ ES5.js
@@ -3,42 +3,46 @@ function Car(weight, volume, power) {
     this.volume = volume;
     this.power = power;
 
-    this.maxSpeed = function () {
-        return Math.floor((this.power / this.weight) * 2000);
-    };
+    this.race = function (dist) {
+        let fuel = this.fuelConsumption() * dist / 100;
+        let fill = 0;
+        let result;
+        while (fuel > this.volume) {
+            fill += fill + 180;
+            fuel -= this.volume;
+        }
+        result = Math.floor((dist / this.maxSpeed()) * 3600 + fill);
 
-    this.secondsToHundred = function () {
-        return Math.round((Math.pow(this.weight / this.power, .75)) * 1000) / 1000;
-    };
-
-    this.fuelConsumption = function () {
-        let res = (this.power * this.weight) / 10000;
-        if (res > 20) return 20;
-        return res
+        let sec_num = parseInt(result, 10);
+        let hours = Math.floor(sec_num / 3600) % 24;
+        let minutes = Math.floor(sec_num / 60) % 60;
+        let seconds = sec_num % 60;
+        let resultOfRace = [hours, minutes, seconds]
+            .map(v => v < 10 ? "0" + v : v)
+            .filter((v, i) => v !== "00" || i > 0)
+            .join(":");
+        return `Вес автомобиля : ${this.weight}кг
+Объем бака : ${this.volume}л.
+Мощность : ${this.power}л.с.
+Максимальная скорость : ${this.maxSpeed()}км/ч
+Разгон до 100км/ч : ${this.secondsToHundred()}сек.
+Расход на 100км : ${this.fuelConsumption()}л.
+Пройдет расстояние в ${dist}км  за ${resultOfRace}`;
     };
 }
 
-Car.prototype.race = function (dist) {
-    let fuel = this.fuelConsumption() * dist / 100;
-    let fill = 0;
-    let result;
-    while (fuel > this.volume) {
-        fill += fill + 180;
-        fuel -= this.volume;
-    }
-    result = Math.floor((dist / this.maxSpeed()) * 3600 + fill);
+Car.prototype.maxSpeed = function () {
+    return Math.floor((this.power / this.weight) * 2000);
+};
 
-    let sec_num = parseInt(result, 10);
-    let hours = Math.floor(sec_num / 3600) % 24;
-    let minutes = Math.floor(sec_num / 60) % 60;
-    let seconds = sec_num % 60;
-    let resultOfRace = [hours, minutes, seconds]
-        .map(v => v < 10 ? "0" + v : v)
-        .filter((v, i) => v !== "00" || i > 0)
-        .join(":");
-    return 'Вес автомобиля : ' + this.weight + 'кг' + '\nОбъем бака : ' + this.volume + 'л.' + '\nМощность : ' + this.power +
-        'л.с.' + '\nМаксимальная скорость : ' + this.maxSpeed() + 'км/ч' + '\nРазгон до 100км/ч : ' + this.secondsToHundred() +
-        'сек.' + '\nРасход на 100км : ' + this.fuelConsumption() + 'л.' + '\nПройдет расстояние в ' + dist + 'км  за ' + resultOfRace;
+Car.prototype.secondsToHundred = function () {
+    return Math.round((Math.pow(this.weight / this.power, .75)) * 1000) / 1000;
+};
+
+Car.prototype.fuelConsumption = function () {
+    let res = (this.power * this.weight) / 10000;
+    if (res > 20) return 20;
+    return res
 };
 
 
@@ -46,3 +50,5 @@ let car1 = new Car(1300, 100, 200);
 let car2 = new Car(1600, 60, 80);
 
 console.log(car1.race(600));
+console.log(car2.race(600));
+console.log(car1);
--- ES6.js
+++ ES6.js
@@ -11,7 +11,7 @@ class Car {
 
     fuelConsumption() {
         let res = (this.power * this.weight) / 10000;
-        if (res > 20) return 20;
+        if (res > 40) return 40;
         return res
     }
 
@@ -29,7 +29,7 @@ class Car {
         }
         result = Math.floor((dist / this.maxSpeed()) * 3600 + fill);
 
-        let sec_num = parseInt(result, 10);
+        let sec_num = parseInt(result);
         let hours = Math.floor(sec_num / 3600) % 24;
         let minutes = Math.floor(sec_num / 60) % 60;
         let seconds = sec_num % 60;
@@ -37,14 +37,21 @@ class Car {
             .map(v => v < 10 ? "0" + v : v)
             .filter((v, i) => v !== "00" || i > 0)
             .join(":");
-        return 'Вес автомобиля : ' + this.weight + 'кг' + '\nОбъем бака : ' + this.volume + 'л.' + '\nМощность : ' + this.power +
-            'л.с.' + '\nМаксимальная скорость : ' + this.maxSpeed() + 'км/ч' + '\nРазгон до 100км/ч : ' + this.secondsToHundred() +
-            'сек.' + '\nРасход на 100км : ' + this.fuelConsumption() + 'л.' + '\nПройдет расстояние в ' + dist + 'км  за ' + resultOfRace;
+        return console.log( `Вес автомобиля : ${this.weight}кг
+Объем бака : ${this.volume}л.
+Мощность : ${this.power}л.с.
+Максимальная скорость : ${this.maxSpeed()}км/ч
+Разгон до 100км/ч : ${this.secondsToHundred()}сек.
+Расход на 100км : ${this.fuelConsumption()}л.
+Пройдет расстояние в ${dist}км  за ${resultOfRace}`);
     }
 }
 
 let car1 = new Car(1300, 100, 200);
 let car2 = new Car(1600, 60, 80);
 
-console.log(car1.race(600));
-console.log(car2.race(600));
+
+
+car1.race(627);
+car2.race(527);
+console.log(car1);
